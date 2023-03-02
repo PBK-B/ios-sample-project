042809c456d61fef1a8e9955a3b22b06cff61942
PBK-B <pbk-b@pbk6.cn>
2023-03-02 10:49:14 +0800

    feat: Increase background process and service detection

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -4,3 +4,11 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.greeting === "hello")
         sendResponse({ farewell: "goodbye" });
 });
+
+console.log("后台服务");
+
+// 启动后台服务
+browser.runtime.sendNativeMessage("application.id", {message: "Hello from background page"}, function(response) {
+    console.log("Received sendNativeMessage response:");
+    console.log(response);
+});
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -4,7 +4,27 @@ console.log("Hello World!", browser);
 //    console.log("Received response: ", response);
 //});
 
-browser.runtime.sendNativeMessage("application.id", {message: "Hello from background page"}, function(response) {
+browser.runtime.sendNativeMessage("application.id", { message: "Hello from background page" }, function (response) {
     console.log("Received sendNativeMessage response:");
     console.log(response);
 });
+
+
+// 定时判断服务是否活跃
+var serverView = document.getElementById("server_s")
+function testServer() {
+    const Http = new XMLHttpRequest();
+    const url = 'http://127.0.0.1:8888';
+    Http.open("GET", url);
+    Http.send();
+
+    Http.onreadystatechange = (e) => {
+        var msg = "异常"
+        if (Http.status == 200) {
+            console.log("请求成功", Http.responseText)
+            msg = "正常"
+        }
+        serverView.innerHTML = `服务器状态(${new Date().getTime()}): ${msg}`
+    }
+}
+setInterval(testServer, 1000);
